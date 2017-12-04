fd445b918dff8e979bac10610f07046d4a97b27f
cuiyonghao <867270105@qq.com>
2017-12-04 21:10:49 +0800

    this is 2017.12.4

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -37,7 +37,8 @@ public class SeckillServiceImpl implements SeckillService {
     /**
      * 使用注解控制事务方法的优点
      * 1：开发团队达成一致约定，明确标注事务方法的编程风格。
-     * 2：保证事务方法的执行时间尽可能短
+     * 2：保证事务方法的执行时间尽可能短，不要穿插其他网络操作RPC/HTTP请求或者剥离到事务方法外部。
+     * 3：不是所有的方法都需要事务，如只有一条修改操作，只读操作不需要事务控制。
      */
    public List<Seckill> getSeckillList() {
         return seckillDao.queryAll(0,4);
---
+++ src/main/java/org/seckill/web/SeckillController.java
@@ -0,0 +1,79 @@
+package org.seckill.web;
+
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.dto.SeckillResult;
+import org.seckill.entity.Seckill;
+import org.seckill.exception.SeckillException;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+    @RequestMapping(name = "/list",method = RequestMethod.GET)
+    public String list(Model model){
+        //获取列表页
+        List<Seckill> list=seckillService.getSeckillList();
+        model.addAttribute("list",list);
+        return "list";//WEB-INF/jsp/"list"/jsp
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail",method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId,Model model) {
+        if (seckillId == null) {
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getById(seckillId);
+        if (seckill == null) {
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("seckill",seckill);
+        return "detail";
+    }
+    //ajax json
+    @RequestMapping(value = "/{seckillId}/exposer",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(Long seckillId){
+        SeckillResult<Exposer> result;
+        try {
+            Exposer exposer= seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true,exposer);
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            result = new SeckillResult<Exposer>(false,e.getMessage());
+        }
+
+        return null;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    public SeckillResult<SeckillException> execute(){
+        SeckillResult<SeckillExecution> result;
+
+        SeckillExecution execution = seckillService.executeSeckill();
+
+        return null;
+    }
+
+
+}
